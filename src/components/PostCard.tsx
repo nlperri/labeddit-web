08ea8c08d87fc94e47ca09df00ac2c4a41c6c40e
natalia <lnataliaperri@gmail.com>
2023-05-24 17:14:06 -0300

    date fn

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,6 +6,10 @@ import ArrowUp from '../assets/arrow-up.svg'
 import ArrowDown from '../assets/arrow-down.svg'
 import CommentsImg from '../assets/comments.svg'
 import React from "react"
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+
 
 interface PostCardProps {
     post: Post
@@ -30,36 +34,48 @@ export function PostCard({ post }: PostCardProps) {
             className="gap-4 p-2 w-full max-w-md bg-grayBg-50 max-h-60 rounded-lg border border-grayBg-200 flex flex-col"
             key={post.id}
         >
+
             <p className="text-sm text-grayBg-300">
                 Enviado por: {post.creator.name}
             </p>
+
+
             <p className="">
                 {post.content}
             </p>
-            <div className="flex gap-2">
-                <div className="p-2 flex items-center justify-around w-24 rounded-full border h-7">
+            <div className="flex justify-between">
+                <div className="flex gap-2">
+                    <div className="p-2 flex items-center justify-around w-24 rounded-full border h-7">
 
-                    <ArrowUp
+                        <ArrowUp
 
-                        className="cursor-pointer fill-pink-500 hover:animate-bounce"
-                        onClick={
-                            () => likeDislikePost({ id: post.id, like: true })
-                        }
-                    />
-                    <p className="text-grayBg-300 text-xs">{post.likes}</p>
-                    <ArrowDown
-                        className="cursor-pointer hover:animate-bounce"
-                        onClick={
-                            () => likeDislikePost({ id: post.id, like: false })
-                        }
-                    />
+                            className="cursor-pointer fill-pink-500 hover:animate-bounce"
+                            onClick={
+                                () => likeDislikePost({ id: post.id, like: true })
+                            }
+                        />
+                        <p className="text-grayBg-300 text-xs">{post.likes}</p>
+                        <ArrowDown
+                            className="cursor-pointer hover:animate-bounce"
+                            onClick={
+                                () => likeDislikePost({ id: post.id, like: false })
+                            }
+                        />
+                    </div>
+                    <div
+                        onClick={() => router.push(`/post/${post.id}`)}
+                        className=" cursor-pointer rounded-full border h-7 py-2 px-3 flex items-center justify-around w-16">
+                        <CommentsImg className="hover:animate-bounce" />
+                        <p className="text-grayBg-300 text-xs">{post.comments.length}</p>
+                    </div>
+
                 </div>
-                <div
-                    onClick={() => router.push(`/post/${post.id}`)}
-                    className=" cursor-pointer rounded-full border h-7 py-2 px-3 flex items-center justify-around w-16">
-                    <CommentsImg className="hover:animate-bounce" />
-                    <p className="text-grayBg-300 text-xs">{post.comments.length}</p>
-                </div>
+                <time className="-ml-8 flex items-center gap-2 text-grayBg-300 text-xs ">
+                    {formatDistanceToNow(new Date(post.createdAt), {
+                        addSuffix: true,
+                        locale: ptBR,
+                    })}
+                </time>
             </div>
 
         </div>
